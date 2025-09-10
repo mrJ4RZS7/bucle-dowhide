bb9c1d92b2723b123a8dcac77108e19adabc7fb8
kevinperezramirez0523-maker <jjpayang0610@gmail.com>
2025-09-10 10:46:49 -0500

    cambios
    validación de información

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ do {
 
     intento = parseInt(prompt( "Adivina el número (1-100)" ));
 
-    if ( intento < numerosecreto ) {
-        alert( "El número es más grande, intenta de nuevo" );
+    if ( intento < 1 || intento > 100 ) {
+        alert ( "El número ingresado no se encuentra dentro del rango establecido; el número ingresado debe ser mayor o igual a 1 y menor o igual a 100. Intenta de nuevo" );
     }
-    else if ( intento > numerosecreto ) {
-        alert( "El número es más pequeño, intenta de nuevo" );
+
+    else {
+        if ( intento < numerosecreto ) {
+            alert( "El número es más grande, intenta de nuevo" );
+        }
+            else if ( intento > numerosecreto ) {
+            alert( "El número es más pequeño, intenta de nuevo" );
+        }
     }
 }
 
